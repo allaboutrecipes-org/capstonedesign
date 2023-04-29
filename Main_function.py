7574229e3a803d040367bfce3f19ae191ea3c436
Lim_Junghwan <124559724+alofrs@users.noreply.github.com>
2023-04-29 22:12:27 +0900

    Update Main_function.py

--- Main_function.py
+++ Main_function.py
@@ -3,4 +3,4 @@ import calculate_module
 
 location = calculate_module.Calculate_location()
 price = calculate_module.Calculate_Price()
-calculate_module.Recommned_Market(price, location)
+calculate_module.Recommend_Market(price, location)
